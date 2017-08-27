c4c0620582a56dbb58f800c5a2e7cfe3392245fa
DeivedLinux <deived.william@live.com>
2017-08-27 11:57:52 -0300

    Five

---
+++ Game.c
@@ -0,0 +1,13 @@
+#include "Game.h"
+
+
+
+static unsigned SCREEN_WIDTH;
+static unsigned SCREEN_HEIGHT;
+
+
+Game* newGame(unsigned width, unsigned height, const SDL_Surface* _background, const SDL_Surface* font)
+{
+
+
+}
--- main.c
+++ main.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Game.h"
+#include <SDL2/SDL.h>
+#include <stdbool.h>
 
-int main(int argc, char const *argv[])
+static const unsigned SCREEN_WIDTH = 480;
+static const unsigned SCREEN_HEIGHT = 270;
+
+int main(int argc, char *argv[])
 {
 
- //The window we'll be rendering to
+
     SDL_Window* window = NULL;
+    bool isRunning = true;
     
-    //The surface contained by the window
+
     SDL_Surface* screenSurface = NULL;
 
-    //Initialize SDL
     if(SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        exit(-1);
+    }
+    else
+    {
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            exit(-1);
+        }
     }
 
-	/* code */
+    while(isRunning)
+    {
+        
+    }
+
+
+
+
+	
 	return 0;
 }
